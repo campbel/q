71c9702eaa4d3ee4d63c99d2a1d838f8ca2b9ebb
Chris Campbell <chris.campbell@dutchie.com>
2024-05-24 23:55:41 -0600

    impl list

---
+++ list.go
@@ -0,0 +1,297 @@
+package q
+
+type List[M any] struct {
+	length int64
+	head   *Node[M]
+	tail   *Node[M]
+}
+
+type Node[M any] struct {
+	value M
+	next  *Node[M]
+	prev  *Node[M]
+}
+
+func NewList[M any](elements ...M) *List[M] {
+	l := &List[M]{}
+	for _, e := range elements {
+		l.Push(e)
+	}
+	return l
+}
+
+func (l *List[M]) Push(values ...M) {
+	for _, v := range values {
+		l.length++
+		node := &Node[M]{value: v}
+		if l.head == nil {
+			l.head = node
+			l.tail = node
+		} else {
+			l.tail.next = node
+			node.prev = l.tail
+			l.tail = node
+		}
+	}
+}
+
+func (l *List[M]) Pop() M {
+	l.length--
+	if l.tail == nil {
+		var m M
+		return m
+	}
+	node := l.tail
+	l.tail = node.prev
+	if l.tail != nil {
+		l.tail.next = nil
+	} else {
+		l.head = nil
+	}
+	return node.value
+}
+
+func (l *List[M]) Extend(lists ...*List[M]) {
+	for _, other := range lists {
+		if other.length == 0 {
+			return
+		}
+		if l.head == nil {
+			l.head = other.head
+			l.tail = other.tail
+		} else {
+			l.tail.next = other.head
+			other.head.prev = l.tail
+			l.tail = other.tail
+		}
+		l.length += other.length
+	}
+}
+
+func (l *List[M]) Shift() M {
+	l.length--
+	if l.head == nil {
+		var m M
+		return m
+	}
+	node := l.head
+	l.head = node.next
+	if l.head != nil {
+		l.head.prev = nil
+	} else {
+		l.tail = nil
+	}
+	return node.value
+}
+
+func (l *List[M]) Unshift(values ...M) {
+	for _, v := range values {
+		l.length++
+		node := &Node[M]{value: v}
+		if l.head == nil {
+			l.head = node
+			l.tail = node
+		} else {
+			l.head.prev = node
+			node.next = l.head
+			l.head = node
+		}
+	}
+}
+
+func (l *List[M]) Reverse() {
+	for n := l.head; n != nil; n = n.prev {
+		n.next, n.prev = n.prev, n.next
+	}
+	l.head, l.tail = l.tail, l.head
+}
+
+func (l *List[M]) Each(callback func(M)) {
+	for n := l.head; n != nil; n = n.next {
+		callback(n.value)
+	}
+}
+
+func (l *List[M]) Find(callback func(M) bool) M {
+	for n := l.head; n != nil; n = n.next {
+		if callback(n.value) {
+			return n.value
+		}
+	}
+	var m M
+	return m
+}
+
+func (l *List[M]) Slice() []M {
+	result := make([]M, 0, l.length)
+	for n := l.head; n != nil; n = n.next {
+		result = append(result, n.value)
+	}
+	return result
+}
+
+func (l *List[M]) Filter(callback func(M) bool) *List[M] {
+	result := &List[M]{}
+	for n := l.head; n != nil; n = n.next {
+		if callback(n.value) {
+			result.Push(n.value)
+		}
+	}
+	return result
+}
+
+func (l *List[M]) All(callback func(M) bool) bool {
+	for n := l.head; n != nil; n = n.next {
+		if !callback(n.value) {
+			return false
+		}
+	}
+	return true
+}
+
+func (l *List[M]) Any(callback func(M) bool) bool {
+	for n := l.head; n != nil; n = n.next {
+		if callback(n.value) {
+			return true
+		}
+	}
+	return false
+}
+
+func IndexOf[M comparable](list *List[M], value M) int {
+	index := 0
+	for n := list.head; n != nil; n = n.next {
+		if n.value == value {
+			return index
+		}
+		index++
+	}
+	return -1
+}
+
+func Delete[M comparable](list *List[M], value M) {
+	for n := list.head; n != nil; n = n.next {
+		if n.value == value {
+			if n == list.head {
+				list.head = n.next
+				n.next.prev = nil
+			} else if n == list.tail {
+				list.tail = n.prev
+				n.prev.next = nil
+			} else {
+				n.prev.next = n.next
+				n.next.prev = n.prev
+			}
+			list.length--
+		}
+	}
+}
+
+func (l *List[M]) Sort(less func(M, M) bool) *List[M] {
+	if l.length < 2 {
+		return l
+	}
+	left, pivot, right := l.partition(less)
+	return stitch(left.Sort(less), pivot, right.Sort(less))
+}
+
+func (l *List[M]) partition(less func(M, M) bool) (*List[M], M, *List[M]) {
+	pivot := l.head.value
+	left := NewList[M]()
+	right := NewList[M]()
+	for n := l.head.next; n != nil; n = n.next {
+		if less(n.value, pivot) {
+			left.Push(n.value)
+		} else {
+			right.Push(n.value)
+		}
+	}
+	return left, pivot, right
+}
+
+func stitch[M any](left *List[M], pivot M, right *List[M]) *List[M] {
+	if left.length == 0 {
+		return Join(NewList[M](pivot), right)
+	}
+	left.Push(pivot)
+	return Join(left, right)
+}
+
+func (l *List[M]) IsSorted(less func(M, M) bool) bool {
+	for n := l.head; n != nil && n.next != nil; n = n.next {
+		if less(n.next.value, n.value) {
+			return false
+		}
+	}
+	return true
+}
+
+func (l *List[M]) Copy() *List[M] {
+	result := &List[M]{}
+	for n := l.head; n != nil; n = n.next {
+		result.Push(n.value)
+	}
+	return result
+}
+
+func (l *List[M]) Len() int {
+	return int(l.length)
+}
+
+func (l *List[M]) Len64() int64 {
+	return l.length
+}
+
+func Map[M any, N any](l *List[M], callback func(M) N) *List[N] {
+	result := &List[N]{}
+	for n := l.head; n != nil; n = n.next {
+		result.Push(callback(n.value))
+	}
+	return result
+}
+
+func Reduce[M any, N any](l *List[M], callback func(N, M) N, initial N) N {
+	acc := initial
+	for n := l.head; n != nil; n = n.next {
+		acc = callback(acc, n.value)
+	}
+	return acc
+}
+
+func Equal[M comparable](a, b *List[M]) bool {
+	if a == nil || b == nil {
+		return a == b
+	}
+	if a.length != b.length {
+		return false
+	}
+	for na, nb := a.head, b.head; na != nil && nb != nil; na, nb = na.next, nb.next {
+		if na.value != nb.value {
+			return false
+		}
+	}
+	return true
+}
+
+func Join[M any](lists ...*List[M]) *List[M] {
+	if len(lists) == 0 {
+		return NewList[M]()
+	}
+
+	if len(lists) == 1 {
+		return lists[0]
+	}
+
+	l := lists[0]
+	for _, list := range lists[1:] {
+		if list.length == 0 {
+			continue
+		}
+		l.tail.next = list.head
+		list.head.prev = l.tail
+		l.tail = list.tail
+		l.length += list.length
+	}
+
+	return l
+}
---
+++ list_test.go
@@ -0,0 +1,285 @@
+package q
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestListPushPop(t *testing.T) {
+	assert := assert.New(t)
+	l := NewList[int]()
+	l.Push(1, 2, 3)
+	assert.Equal(3, l.Pop())
+	assert.Equal(2, l.Pop())
+	assert.Equal(1, l.Pop())
+	assert.Zero(l.Pop())
+}
+
+func TestListShift(t *testing.T) {
+	assert := assert.New(t)
+	l := NewList[int]()
+	l.Push(1, 2, 3)
+	assert.Equal(1, l.Shift())
+	assert.Equal(2, l.Shift())
+	assert.Equal(3, l.Shift())
+	assert.Zero(l.Shift())
+}
+
+func TestListUnshift(t *testing.T) {
+	assert := assert.New(t)
+	l := NewList[int]()
+	l.Unshift(1, 2, 3)
+	assert.Equal(3, l.Shift())
+	assert.Equal(2, l.Shift())
+	assert.Equal(1, l.Shift())
+	assert.Zero(l.Pop())
+}
+
+func TestListLength(t *testing.T) {
+	assert := assert.New(t)
+	l := NewList[int]()
+	l.Push(1, 2, 3)
+	assert.Equal(int64(3), l.Len64())
+	assert.Equal(3, l.Len())
+}
+
+func TestSort(t *testing.T) {
+	assert := assert.New(t)
+	list := NewList[int]()
+	count := 10000
+	for i := 0; i < count; i++ {
+		list.Push(rand.Int())
+	}
+	assert.False(list.IsSorted(LessInt))
+	list = list.Sort(LessInt)
+	assert.Equal(count, list.Len())
+	assert.True(list.IsSorted(LessInt))
+}
+
+func LessInt(a, b int) bool {
+	return a < b
+}
+
+func TestListReverse(t *testing.T) {
+	assert := assert.New(t)
+	l := NewList[int]()
+	l.Push(1, 2, 3)
+	l.Reverse()
+	assert.Equal(3, l.Shift())
+	assert.Equal(2, l.Shift())
+	assert.Equal(1, l.Shift())
+	assert.Zero(l.Shift())
+}
+
+func TestListJoin(t *testing.T) {
+	assert := assert.New(t)
+	l1 := NewList[int]()
+	l1.Push(1, 2, 3)
+	l2 := NewList[int]()
+	l2.Push(4, 5, 6)
+	l3 := Join[int](l1, l2)
+	assert.Equal(6, l3.Len())
+	assert.Equal(1, l3.Shift())
+	assert.Equal(2, l3.Shift())
+	assert.Equal(3, l3.Shift())
+	assert.Equal(4, l3.Shift())
+	assert.Equal(5, l3.Shift())
+	assert.Equal(6, l3.Shift())
+	assert.Zero(l3.Shift())
+}
+
+func TestReduce(t *testing.T) {
+	assert := assert.New(t)
+	l := NewList[int]()
+	l.Push(1, 2, 3)
+	sum := Reduce(l, func(acc, value int) int {
+		return acc + value
+	}, 0)
+	assert.Equal(6, sum)
+}
+
+func TestMap(t *testing.T) {
+	assert := assert.New(t)
+	l := NewList[int]()
+	l.Push(1, 2, 3)
+	l = Map[int, int](l, func(value int) int {
+		return value * 2
+	})
+	assert.Equal(2, l.Shift())
+	assert.Equal(4, l.Shift())
+	assert.Equal(6, l.Shift())
+	assert.Zero(l.Shift())
+}
+
+func TestListEach(t *testing.T) {
+	assert := assert.New(t)
+	l := NewList[int]()
+	l.Push(1, 2, 3)
+	sum := 0
+	l.Each(func(value int) {
+		sum += value
+	})
+	assert.Equal(6, sum)
+}
+
+func TestListFind(t *testing.T) {
+	assert := assert.New(t)
+	l := NewList[int]()
+	l.Push(1, 2, 3)
+	value := l.Find(func(value int) bool {
+		return value == 2
+	})
+	assert.Equal(2, value)
+
+	zero := l.Find(func(value int) bool {
+		return false
+	})
+	assert.Zero(zero)
+}
+
+func TestListFilter(t *testing.T) {
+	assert := assert.New(t)
+	l := NewList[int]()
+	l.Push(1, 2, 3)
+	l = l.Filter(func(value int) bool {
+		return value%2 == 0
+	})
+	assert.Equal(1, l.Len())
+	assert.Equal(2, l.Shift())
+	assert.Zero(l.Shift())
+}
+
+func TestListEqual(t *testing.T) {
+	assert := assert.New(t)
+	l1 := NewList(1, 2, 3)
+	l2 := NewList(1, 2, 3)
+	assert.True(Equal(l1, l2))
+	l2.Push(4)
+	assert.False(Equal(l1, l2))
+	l1.Push(5)
+	assert.False(Equal(l1, l2))
+	assert.True(Equal(NewList[int](), NewList[int]()))
+	assert.False(Equal(NewList[int](), nil))
+	l3 := l1.Copy()
+	assert.True(Equal(l1, l3))
+}
+
+func TestJoin(t *testing.T) {
+	assert := assert.New(t)
+	l1 := NewList(1, 2, 3)
+	l2 := NewList(4, 5, 6)
+	l3 := Join(l1, l2)
+	assert.Equal(6, l3.Len())
+	assert.Equal(1, l3.Shift())
+	assert.Equal(2, l3.Shift())
+	assert.Equal(3, l3.Shift())
+	assert.Equal(4, l3.Shift())
+	assert.Equal(5, l3.Shift())
+	assert.Equal(6, l3.Shift())
+	assert.Zero(l3.Shift())
+
+	l4 := NewList(1)
+	l5 := Join(l4)
+	assert.Equal(l4, l5)
+
+	l6 := Join[int]()
+	assert.Equal(NewList[int](), l6)
+}
+
+func TestListExtend(t *testing.T) {
+	assert := assert.New(t)
+	l1 := NewList(1, 2, 3)
+	l2 := NewList(4, 5, 6)
+	l1.Extend(l2)
+	assert.Equal(6, l1.Len())
+	assert.Equal(1, l1.Shift())
+	assert.Equal(2, l1.Shift())
+	assert.Equal(3, l1.Shift())
+	assert.Equal(4, l1.Shift())
+	assert.Equal(5, l1.Shift())
+	assert.Equal(6, l1.Shift())
+	assert.Zero(l1.Shift())
+
+	l3 := NewList(1)
+	l4 := NewList(2)
+	l3.Extend(l4)
+	assert.Equal(2, l3.Len())
+	assert.Equal(1, l3.Shift())
+	assert.Equal(2, l3.Shift())
+	assert.Zero(l3.Shift())
+
+	l5 := NewList(1)
+	l6 := NewList[int]()
+	l5.Extend(l6)
+	assert.Equal(1, l5.Len())
+	assert.Equal(1, l5.Shift())
+	assert.Zero(l5.Shift())
+
+	l7 := NewList[int]()
+	l8 := NewList(1)
+	l7.Extend(l8)
+	assert.Equal(1, l7.Len())
+	assert.Equal(1, l7.Shift())
+	assert.Zero(l7.Shift())
+}
+
+func TestAll(t *testing.T) {
+	assert := assert.New(t)
+	l := NewList(1, 2, 3)
+	assert.True(l.All(func(value int) bool {
+		return value > 0
+	}))
+	assert.False(l.All(func(value int) bool {
+		return value > 1
+	}))
+}
+
+func TestAny(t *testing.T) {
+	assert := assert.New(t)
+	l := NewList(1, 2, 3)
+	assert.True(l.Any(func(value int) bool {
+		return value > 2
+	}))
+	assert.False(l.Any(func(value int) bool {
+		return value > 3
+	}))
+}
+
+func TestIndexOf(t *testing.T) {
+	assert := assert.New(t)
+	l := NewList(1, 2, 3)
+	assert.Equal(1, IndexOf(l, 2))
+	assert.Equal(-1, IndexOf(l, 4))
+}
+
+func TestDelete(t *testing.T) {
+	assert := assert.New(t)
+	l := NewList(1, 2, 3)
+	Delete(l, 2)
+	assert.Equal(2, l.Len())
+	assert.Equal(1, l.Shift())
+	assert.Equal(3, l.Shift())
+	assert.Zero(l.Shift())
+
+	l = NewList(1, 2, 3)
+	Delete(l, 1)
+	assert.Equal(2, l.Len())
+	assert.Equal(2, l.Shift())
+	assert.Equal(3, l.Shift())
+	assert.Zero(l.Shift())
+
+	l = NewList(1, 2, 3)
+	Delete(l, 3)
+	assert.Equal(2, l.Len())
+	assert.Equal(1, l.Shift())
+	assert.Equal(2, l.Shift())
+	assert.Zero(l.Shift())
+}
+
+func TestSlice(t *testing.T) {
+	assert := assert.New(t)
+	l := NewList(1, 2, 3, 4, 5).Slice()
+	assert.Equal([]int{1, 2, 3, 4, 5}, l)
+}
