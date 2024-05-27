89dcb7a66f3ec40371dd049481670b4c5bc95634
Chris Campbell <chris.campbell@dutchie.com>
2024-05-27 15:40:20 -0600

    self identifier change

--- counter.go
+++ counter.go
@@ -17,49 +17,49 @@ func NewCounter[T comparable](elements ...T) *Counter[T] {
 
 // Add adds one or more elements to the counter.
 // Time complexity: O(n), where n is the number of elements being added.
-func (s *Counter[T]) Add(elements ...T) {
+func (c *Counter[T]) Add(elements ...T) {
 	for _, element := range elements {
-		s.data[element]++
+		c.data[element]++
 	}
 }
 
 // Remove removes an element from the counter.
 // Time complexity: O(1).
-func (s *Counter[T]) Remove(element T) {
-	delete(s.data, element)
+func (c *Counter[T]) Remove(element T) {
+	delete(c.data, element)
 }
 
 // Contains checks if an element is present in the counter.
 // Time complexity: O(1).
-func (s *Counter[T]) Contains(element T) bool {
-	_, exists := s.data[element]
+func (c *Counter[T]) Contains(element T) bool {
+	_, exists := c.data[element]
 	return exists
 }
 
-func (s *Counter[T]) Count(element T) int {
-	if s.Contains(element) {
-		return s.data[element]
+func (c *Counter[T]) Count(element T) int {
+	if c.Contains(element) {
+		return c.data[element]
 	}
 	return 0
 }
 
 // Len returns the number of elements in the counter.
 // Time complexity: O(1).
-func (s *Counter[T]) Len() int {
-	return len(s.data)
+func (c *Counter[T]) Len() int {
+	return len(c.data)
 }
 
 // Clear removes all elements from the counter.
 // Time complexity: O(1).
-func (s *Counter[T]) Clear() {
-	s.data = make(map[T]int)
+func (c *Counter[T]) Clear() {
+	c.data = make(map[T]int)
 }
 
 // Elements returns a slice containing all the elements in the counter.
 // Time complexity: O(n), where n is the number of elements in the counter.
-func (s *Counter[T]) Elements() []T {
-	elements := make([]T, 0, len(s.data))
-	for element := range s.data {
+func (c *Counter[T]) Elements() []T {
+	elements := make([]T, 0, len(c.data))
+	for element := range c.data {
 		elements = append(elements, element)
 	}
 	return elements
@@ -67,11 +67,11 @@ func (s *Counter[T]) Elements() []T {
 
 // Equal checks if the counter is equal to another counter.
 // Time complexity: O(n), where n is the number of elements in the counter.
-func (s *Counter[T]) Equal(other *Counter[T]) bool {
-	if s.Len() != other.Len() {
+func (c *Counter[T]) Equal(other *Counter[T]) bool {
+	if c.Len() != other.Len() {
 		return false
 	}
-	for element, count := range s.data {
+	for element, count := range c.data {
 		if other.Count(element) != count {
 			return false
 		}
@@ -79,6 +79,6 @@ func (s *Counter[T]) Equal(other *Counter[T]) bool {
 	return true
 }
 
-func (s *Counter[T]) String() string {
-	return fmt.Sprintf("%v", s.data)
+func (c *Counter[T]) String() string {
+	return fmt.Sprintf("%v", c.data)
 }
